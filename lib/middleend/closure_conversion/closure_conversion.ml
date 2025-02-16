60d8a3c7f55480950bc29ce614d273e128234f02
PavlushaSource <pavlushkin.dima1785@gmail.com>
2025-02-17 02:11:33 +0300

    feat: Add CC for Nonrec let [Not working]

--- lib/middleend/closure_conversion/closure_conversion.ml
+++ lib/middleend/closure_conversion/closure_conversion.ml
@@ -13,54 +13,164 @@ open LMisc
 open LAst
 open Common
 
-let emptyEnv = Map.empty (module String)
+let rec cc_expr global_env env_map = function
+  | Expr.Id (I id) as orig -> (
+    match Map.find env_map id with
+    | Some vals_to_apply ->
+        List.fold vals_to_apply ~init:orig ~f:(fun orig val_to_apply ->
+            Expr.Apply (orig, Expr.Id (I val_to_apply)) )
+    | None ->
+        orig )
+  | Expr.Const _ as orig ->
+      orig
+  | Expr.Let (Rec, bindings, body) as orig ->
+      orig
+  | Expr.Let (Nonrec, bindings, body) ->
+      let bindings_list = List1.to_list bindings in
+      let first_binding = List.hd_exn bindings_list in
+      let vars_to_add = Set.elements (find_args first_binding.expr) in
+      let new_value_binding =
+        match vars_to_add with
+        | [] ->
+            Expr.
+              { pat= first_binding.pat
+              ; expr= cc_expr global_env env_map first_binding.expr }
+        | _ -> (
+            let pattern_to_add =
+              List.fold vars_to_add ~init:[] ~f:(fun acc var ->
+                  Pat.Var (I var) :: acc )
+            in
+            let pattern_to_add = List.rev pattern_to_add in
+            match first_binding.expr with
+            | Expr.Fun (patterns, inner_body) ->
+                let all_patterns =
+                  List.append pattern_to_add (List1.to_list patterns)
+                in
+                Expr.
+                  { pat= first_binding.pat
+                  ; expr=
+                      Expr.Fun
+                        ( List1.of_list_exn all_patterns
+                        , cc_expr global_env env_map inner_body ) }
+            | _ ->
+                let new_expr = cc_expr global_env env_map first_binding.expr in
+                Expr.{pat= first_binding.pat; expr= new_expr} )
+      in
+      let pattern_args = Set.elements @@ get_pattern_arg first_binding.pat in
+      let new_env_map =
+        List.fold pattern_args ~init:env_map ~f:(fun acc new_var ->
+            Map.add_exn acc ~key:new_var ~data:vars_to_add )
+      in
+      let new_global_env = List.fold pattern_args ~init:global_env ~f:Set.add in
+      let new_body = cc_expr new_global_env new_env_map body in
+      Expr.Let (Nonrec, List1.of_list_exn [new_value_binding], new_body)
+  | Expr.Fun (args, body) as orig -> (
+      let pattern_list = List1.to_list args in
+      let patterns_args = get_patterns_arg_list pattern_list in
+      let free_vars = Set.diff (find_args body) patterns_args in
+      let free_vars = Set.diff free_vars global_env in
+      let closed_args =
+        Set.fold free_vars ~init:pattern_list ~f:(fun acc id ->
+            Pat.Var (I id) :: acc )
+      in
+      let body = cc_expr global_env env_map body in
+      match closed_args with
+      | [] ->
+          orig
+      | hd :: tl ->
+          Set.fold_right free_vars
+            ~init:(Expr.Fun (List1.of_list_exn (hd :: tl), body))
+            ~f:(fun var acc -> Expr.Apply (acc, Expr.Id (I var))) )
+  | Expr.Function cases ->
+      let new_cases =
+        List.fold (List1.to_list cases) ~init:[] ~f:(fun acc case ->
+            let pattern_args = Set.elements @@ get_pattern_arg case.left in
+            let new_env_map =
+              List.fold pattern_args ~init:env_map ~f:(fun acc new_var ->
+                  Map.remove acc new_var )
+            in
+            let new_case_expr = cc_expr global_env new_env_map case.right in
+            Expr.{left= case.left; right= new_case_expr} :: acc )
+      in
+      Expr.Function (List1.of_list_exn @@ List.rev new_cases)
+  | Expr.Apply (func, arg) ->
+      let func = cc_expr global_env env_map func in
+      let arg = cc_expr global_env env_map arg in
+      Expr.Apply (func, arg)
+  | Expr.Match (expr, cases) ->
+      let new_expr = cc_expr global_env env_map expr in
+      let new_cases =
+        List.fold (List1.to_list cases) ~init:[] ~f:(fun acc case ->
+            let pattern_args = Set.elements @@ get_pattern_arg case.left in
+            let new_env_map =
+              List.fold pattern_args ~init:env_map ~f:(fun acc new_var ->
+                  Map.remove acc new_var )
+            in
+            let new_case_expr = cc_expr global_env new_env_map case.right in
+            Expr.{left= case.left; right= new_case_expr} :: acc )
+      in
+      Expr.Match (new_expr, List1.of_list_exn @@ List.rev new_cases)
+  | Expr.If (condition, if_expr, else_expr) ->
+      let new_condition = cc_expr global_env env_map condition in
+      let new_if_expr = cc_expr global_env env_map if_expr in
+      let new_else_expr =
+        Option.map ~f:(cc_expr global_env env_map) else_expr
+      in
+      Expr.If (new_condition, new_if_expr, new_else_expr)
+  | Expr.Construct _ ->
+      Expr.Id (I "Don't have ideas")
+  | Expr.Tuple expr_list2 ->
+      let new_expr_list =
+        List.map (List2.to_list expr_list2) ~f:(cc_expr global_env env_map)
+      in
+      Expr.Tuple (List2.of_list_exn new_expr_list)
+  | Expr.Seq expr_list2 ->
+      let new_expr_list =
+        List.map (List2.to_list expr_list2) ~f:(cc_expr global_env env_map)
+      in
+      Expr.Seq (List2.of_list_exn new_expr_list)
+  | Expr.Constraint (expr, t) ->
+      let new_expr = cc_expr global_env env_map expr in
+      Expr.Constraint (new_expr, t)
 
-let empty = Set.empty (module String)
-
-let rec get_identifiers = function
-  | Pat.Any | Pat.Const _ ->
-      empty
-  | Pat.Var (I id) ->
-      Set.singleton (module String) id
-  | Pat.Tuple patterns ->
-      List.fold (List2.to_list patterns) ~init:empty ~f:(fun acc pat ->
-          Set.union acc (get_identifiers pat) )
-  | Pat.Or (l, r) ->
-      Set.union (get_identifiers l) (get_identifiers r)
-  | Pat.Construct _ ->
-      empty
-  | Pat.Constraint (pat, _) ->
-      get_identifiers pat
-
-let emptyEnv = Map.empty (module String)
-
-let empty = Set.empty (module String)
-
-let cc_structure_item env global_scope = function
+let cc_structure_item global_env = function
+  | StrItem.Type _ as orig ->
+      (global_env, orig)
   | StrItem.Eval expr ->
-      (env, StrItem.Eval expr)
+      (global_env, StrItem.Eval (cc_expr global_env empty_map expr))
   | StrItem.Let (Rec, bindings) ->
-      (env, StrItem.Let (Rec, bindings))
+      let pattern_list = get_pattern_list_from_bindings bindings in
+      let pattern_args = Set.elements @@ get_patterns_arg_list pattern_list in
+      let global_env = List.fold pattern_args ~init:global_env ~f:Set.add in
+      let bindings =
+        List.fold (List1.to_list bindings) ~init:[] ~f:(fun acc binding ->
+            match binding.expr with
+            | Expr.Fun (patterns, body) ->
+                let expr = cc_expr global_env empty_map body in
+                Expr.{pat= binding.pat; expr= Expr.Fun (patterns, expr)} :: acc
+            | _ ->
+                let expr = cc_expr global_env empty_map binding.expr in
+                Expr.{pat= binding.pat; expr} :: acc )
+      in
+      (global_env, StrItem.Let (Rec, List1.of_list_exn bindings))
   | StrItem.Let (Nonrec, bindings) ->
-      (* let idents = get_identifiers bindings in
-
-         (env, )
-         List.map (List1.to_list bindings)
-           ~f:(fun {pat; expr} ->
-             group @@ nest 2 @@ ppat pat ^^ group (string " =" ^/^ pexpr expr) ) *)
-      (env, StrItem.Let (Nonrec, bindings))
-  | StrItem.Type {id; params; variants} ->
-      (env, StrItem.Type {id; params; variants})
+      let bindings =
+        List.fold (List1.to_list bindings) ~init:[] ~f:(fun acc binding ->
+            let pat = binding.pat in
+            let expr = cc_expr global_env empty_map binding.expr in
+            Expr.{pat; expr} :: acc )
+      in
+      (global_env, StrItem.Let (Nonrec, List1.of_list_exn bindings))
 
 let run_closure_conversion program =
-  let rec helper env = function
+  let rec helper global_env = function
     | [] ->
         []
     | hd :: tl ->
-        let cur_env, cur_ast = cc_structure_item env empty hd in
-        cur_ast :: helper cur_env tl
+        let new_global_env, new_str = cc_structure_item global_env hd in
+        new_str :: helper new_global_env tl
   in
-  helper emptyEnv program
+  helper empty_global_scope program
 
 let run_cc s =
   match LParse.parse s with
@@ -72,9 +182,15 @@ let run_cc s =
 
 let%expect_test _ =
   run_cc {|let (c, d) = (1, 2) in fun a -> a + c + d|} ;
-  [%expect {|
-    let c, d = 1, 2 in
-    fun a -> (+) ((+) a c) d
-    |}]
+  [%expect
+    {|
+       let c, d = 1, 2 in
+       (fun (+) a -> (+) ((+) a c) d) (+)
+       |}]
 
-(* должно быть  (((fun c d a -> ((+ ((+ a) c)) d)) c) d) *)
+let%expect_test _ =
+  run_cc {| let a = fun x -> x + 2|} ;
+  [%expect
+    {|
+       let a = (fun (+) x -> (+) x 2) (+)
+       |}]
--- lib/middleend/closure_conversion/common.ml
+++ lib/middleend/closure_conversion/common.ml
@@ -7,42 +7,98 @@
 [@@@ocaml.text "/*"]
 
 open! Base
+open LAst
+open LMisc
 
-let emptyEnv = Map.empty (module String)
+let empty_global_scope =
+  let init = Base.Set.empty (module String) in
+  let init = Set.add init "( * )" in
+  let init = Set.add init "( / )" in
+  let init = Set.add init "( + )" in
+  let init = Set.add init "( - )" in
+  let init = Set.add init "( = )" in
+  let init = Set.add init "( == )" in
+  let init = Set.add init "( <> )" in
+  let init = Set.add init "( != )" in
+  let init = Set.add init "( < )" in
+  let init = Set.add init "( <= )" in
+  let init = Set.add init "( > )" in
+  let init = Set.add init "( >= )" in
+  let init = Set.add init "( && )" in
+  let init = Set.add init "( || )" in
+  let init = Set.add init "print_int" in
+  let init = Set.add init "print_string" in
+  let init = Set.add init "( ~+ )" in
+  let init = Set.add init "( ~- )" in
+  init
 
-let emptyGlobalScope = Set.empty (module String)
+let empty_set = Base.Set.empty (module Base.String)
 
-module StrSet : sig
-  type t
+let empty_map = Map.empty (module String)
 
-  val empty : t
-  val singleton : string -> t
-  val union : t -> t -> t
-  val union_list : t list -> t
-  val add : t -> string -> t
-  val find : t -> string -> bool
-  val to_list : t -> string list
-  val of_list : string list -> t
-  val fold : t -> init:'a -> f:('a -> string -> 'a) -> 'a
-  val diff : t -> t -> t
-end = struct
-  type t = (string, String.comparator_witness) Set.t
+let rec get_pattern_arg = function
+  | Pat.Any | Pat.Const _ ->
+      empty_set
+  | Pat.Var (I id) ->
+      Set.singleton (module String) id
+  | Pat.Tuple patterns ->
+      List.fold (List2.to_list patterns) ~init:empty_set ~f:(fun acc pat ->
+          Set.union acc (get_pattern_arg pat) )
+  | Pat.Or (l, r) ->
+      Set.union (get_pattern_arg l) (get_pattern_arg r)
+  | Pat.Construct _ ->
+      empty_set
+  | Pat.Constraint (pat, _) ->
+      get_pattern_arg pat
 
-  let empty = Set.empty (module String)
-  let singleton str = Set.singleton (module String) str
-  let union = Set.union
-  let union_list lst = Set.union_list (module String) lst
+let get_patterns_arg_list patterns =
+  List.fold patterns ~init:empty_set ~f:(fun acc pat ->
+      Set.union acc (get_pattern_arg pat) )
 
-  let find s str =
-    match Set.binary_search s ~compare:String.compare `First_equal_to str with
-    | Some _ ->
-        true
-    | None ->
-        false
+let find_args =
+  let rec find_args_in_binding acc (binding : Expr.value_binding) =
+    match binding with
+    | {pat; expr} ->
+        let pat_args = get_pattern_arg pat in
+        Set.diff (helper acc expr) pat_args
+  and helper acc = function
+    | Expr.Id (I id) ->
+        Set.add acc id
+    | Expr.Const _ ->
+        acc
+    | Expr.Let (_, bindings, body) ->
+        let bindings_list = List1.to_list bindings in
+        let acc = List.fold bindings_list ~init:acc ~f:find_args_in_binding in
+        helper acc body
+    | Expr.Fun (patterns, body) ->
+        let pat_args = get_patterns_arg_list (List1.to_list patterns) in
+        Set.diff (helper acc body) pat_args
+    | Expr.Function cases ->
+        List.fold (List1.to_list cases) ~init:acc ~f:(fun acc case ->
+            Set.diff (helper acc case.right) (get_pattern_arg case.left) )
+    | Expr.Apply (l, r) ->
+        helper (helper acc l) r
+    | Expr.Match (match_expr, cases) ->
+        let acc = helper acc match_expr in
+        List.fold (List1.to_list cases) ~init:acc ~f:(fun acc case ->
+            Set.diff (helper acc case.right) (get_pattern_arg case.left) )
+    | Expr.Tuple exprs ->
+        List.fold (List2.to_list exprs) ~init:acc ~f:helper
+    | Expr.Construct (I id, expr) -> (
+        let acc = Set.add acc id in
+        match expr with Some expr -> helper acc expr | None -> acc )
+    | Expr.If (condition, if_expr, else_expr) -> (
+        let acc = helper acc condition in
+        let acc = helper acc if_expr in
+        match else_expr with Some expr -> helper acc expr | None -> acc )
+    | Expr.Seq exprs ->
+        List.fold (List2.to_list exprs) ~init:acc ~f:helper
+    | Expr.Constraint (expr, _) ->
+        helper acc expr
+  in
+  helper empty_set
 
-  let add = Set.add
-  let to_list = Set.to_list
-  let of_list = Set.of_list (module String)
-  let fold = Set.fold
-  let diff = Set.diff
-end
+let get_pattern_list_from_bindings (bindings : Expr.value_binding List1.t) =
+  List.fold (List1.to_list bindings) ~init:[] ~f:(fun acc binding ->
+      let pat = binding.pat in
+      pat :: acc )
