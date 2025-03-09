b3dca09cfc09e9405bbeeac9a1d4833efc50b49c
Andrei <therain.i@yahoo.com>
2025-03-09 20:33:02 +0300

    feat(middle): introduce `C`losure`Less` IR

--- lib/middle/test/MTest.ml
+++ lib/middle/test/MTest.ml
@@ -13,29 +13,52 @@ open LMiddle
 
 module Format = Stdlib.Format
 
-let run s =
-  match LParse.parse s with
-  | None ->
+type err = ParseError | SimplError of MSimpl.err
+
+let run' (ir : [`Simpl | `CLess]) s =
+  let globals = IdSet.of_list [I "+"] in
+
+  let open Result in
+  let ( let* ) = ( >>= ) in
+
+  let* structure =
+    LParse.parse s |> Option.value_map ~default:(fail ParseError) ~f:return
+  in
+
+  let* sim =
+    MSimpl.from_structure structure |> map_error ~f:(fun err -> SimplError err)
+  in
+
+  let print = PPrint.ToChannel.pretty 1. 50 stdout in
+  ( match ir with
+  | `Simpl ->
+      LPrint.pp_expr (MSimpl.to_expr sim) |> print
+  | `CLess ->
+      let cls = MCLess.from_simpl globals sim in
+      LPrint.pp_structure (MCLess.to_structure cls) |> print ) ;
+
+  return ()
+
+let run ir s =
+  match run' ir s with
+  | Error ParseError ->
       print_endline "syntax error"
-  | Some str -> (
-    match MSimpl.from_structure str with
-    | Error err ->
-        MSimpl.pp_err Format.std_formatter err
-    | Ok sim ->
-        PPrint.ToChannel.pretty 1. 40 stdout
-          (LPrint.pp_expr (MSimpl.to_expr sim)) )
+  | Error (SimplError err) ->
+      MSimpl.pp_err Format.std_formatter err
+  | Ok _ ->
+      ()
 
-let%expect_test _ = run {| let a = 1 |} ; [%expect {| (fun a -> ()) 1 |}]
+let%expect_test _ = run `Simpl {| let a = 1 |} ; [%expect {| (fun a -> ()) 1 |}]
 
 let%expect_test _ =
-  run {| let 1 = 2 |} ; [%expect {| (NotImplemented "patterns") |}]
+  run `Simpl {| let 1 = 2 |} ; [%expect {| (NotImplemented "patterns") |}]
 
 let%expect_test _ =
-  run {| let a = 1 in let b = 2 in b |} ;
+  run `Simpl {| let a = 1 in let b = 2 in b |} ;
   [%expect {| (fun a -> (fun b -> b) 2) 1; () |}]
 
 let%expect_test _ =
-  run
+  run `Simpl
     {| let a = 1 and b = 2 in
        let f x y = x + y in
        f 5;;
@@ -46,16 +69,13 @@ let%expect_test _ =
     {|
     (
      fun b ->
-       (
-        fun a ->
-          (fun f -> f 5)
-            (fun x y -> (+) x y)
-       ) 1
+       (fun a -> (fun f -> f 5) (fun x y -> (+) x y))
+         1
     ) 2; ((+) a b; ())
     |}]
 
 let%expect_test _ =
-  run
+  run `Simpl
     {| let f x y = x + y;;
        f 1 1;;
        let f x y = x - y;;
@@ -65,7 +85,42 @@ let%expect_test _ =
     (
      fun f ->
        f 1 1;
-       (fun f -> f 2 2; ())
-         (fun x y -> (-) x y)
+       (fun f -> f 2 2; ()) (fun x y -> (-) x y)
     ) (fun x y -> (+) x y)
+    |}]
+
+let%expect_test _ =
+  run `CLess
+    {| let f x y =
+       let x z = y + z in
+       let y z = x 1 + z in
+       x 1 + y 2 |} ;
+  [%expect
+    {|
+    let f0 = fun y z -> (+) y z;;
+    let f1 = fun x z -> (+) (x 1) z;;
+    let f2 = fun x y -> (+) (x 1) (y 2);;
+    let f3 = fun x -> f2 x (f1 x);;
+    let f4 = fun x y -> f3 (f0 y);;
+    let f5 = fun f -> ();;
+    f5 f4
+    |}]
+
+let%expect_test _ =
+  run `CLess {| let x = 1 + 2 and y = 3 and z = 4 in x |} ;
+  [%expect
+    {|
+    let f0 = fun x -> x;;
+    let f1 = fun y -> f0 ((+) 1 2);;
+    let f2 = fun z -> f1 3;;
+    f2 4; ()
+    |}]
+
+let%expect_test _ =
+  run `CLess {| let x = 1;; let y = 2;; x + y|} ;
+  [%expect
+    {|
+    let f0 = fun x y -> (+) x y; ();;
+    let f1 = fun x -> f0 x 2;;
+    f1 1
     |}]
