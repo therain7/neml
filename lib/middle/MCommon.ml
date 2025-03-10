eef6b9572d665c485bc2d57cae897717b57342c3
Andrei <therain.i@yahoo.com>
2025-03-10 13:47:34 +0300

    refactor(middle): (re)factor out `FuncDef`

--- lib/middle/MCommon.ml
+++ lib/middle/MCommon.ml
@@ -9,6 +9,7 @@
 open! Base
 
 open LMisc
+open LAst
 
 module IdSet = struct
   type t = (Id.t, Id.comparator_witness) Set.t
@@ -17,4 +18,16 @@ module IdSet = struct
   let single x : t = Set.singleton (module Id) x
 
   let of_list : Id.t list -> t = Set.of_list (module Id)
+end
+
+module FuncDef = struct
+  type 'a t =
+    | Func of {recf: Expr.rec_flag; id: Id.t; args: Id.t List1.t; body: 'a}
+
+  let to_stritem (to_expr : 'a -> Expr.t) (Func {recf; id; args; body} : 'a t) :
+      StrItem.t =
+    let efunc : Expr.t =
+      Fun (List1.map args ~f:(fun id -> Pat.Var id), to_expr body)
+    in
+    Let (recf, List1.of_list_exn [Expr.{pat= Pat.Var id; expr= efunc}])
 end
