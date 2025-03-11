cf147debde83e781aa03b619da035329a5e957a4
Andrei <therain.i@yahoo.com>
2025-03-12 02:37:49 +0300

    feat: simple llvm backend !!
    Co-authored-by: PavlushaSource <pavlushkin.dima1785@gmail.com>

---
+++ lib/back/LBack.ml
@@ -0,0 +1,33 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2024, Andrei, PavlushaSource *)
+
+(** SPDX-License-Identifier: MIT *)
+
+[@@@ocaml.text "/*"]
+
+open! Base
+
+module LLId = BCommon.LLId
+module LLRuntime = BRuntime.LLRuntime
+
+module BCodegen : sig
+  open Llvm
+
+  type err = TypeError of string | NotImplemented of string
+  type builtin = llmodule -> LLId.t * llvalue * lltype
+
+  module LLCodeGen : functor
+    (_ : sig
+       val lmod : llmodule
+     end)
+    (_ : sig
+       val create_closure : func:llvalue -> argc:int -> llbuilder -> llvalue
+       val apply_closure :
+         closure:llvalue -> args:llvalue list -> llbuilder -> llvalue
+     end)
+    -> sig
+    val gen : builtins:builtin list -> LMiddle.MAnf.t -> (unit, err) Result.t
+  end
+end =
+  BCodegen
