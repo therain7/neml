36eb60641ef93c72ad996c84e29b2bd73e023bf4
Andrei <therain.i@yahoo.com>
2025-03-12 14:49:23 +0300

    feat(builtin): add `print_int`
    Co-authored-by: PavlushaSource <pavlushkin.dima1785@gmail.com>

--- lib/back/BRuntime.ml
+++ lib/back/BRuntime.ml
@@ -9,44 +9,52 @@
 open! Base
 open Llvm
 
-module LLRuntime (LLModule : sig
+module Make (LLModule : sig
   val lmod : llmodule
 end) : sig
   val create_closure : func:llvalue -> argc:int -> llbuilder -> llvalue
   val apply_closure :
     closure:llvalue -> args:llvalue list -> llbuilder -> llvalue
+  val print_int : llvalue -> llbuilder -> llvalue
 end = struct
   let lmod = LLModule.lmod
   let i64 = i64_type (module_context lmod)
 
   module Func = struct
     module T = struct
-      type t = CreateClosure | ApplyClosure [@@deriving ord, sexp_of]
+      type t = CreateClosure | ApplyClosure | PrintInt
+      [@@deriving ord, sexp_of]
     end
     include T
     include Comparator.Make (T)
   end
 
+  let declare id typ =
+    let func = declare_function id typ lmod in
+    (func, typ)
+
   let funcs =
     Map.of_alist_exn
       (module Func)
-      [ (CreateClosure, ("neml_create_closure", function_type i64 [|i64; i64|]))
+      [ ( CreateClosure
+        , declare "neml_create_closure" (function_type i64 [|i64; i64|]) )
       ; ( ApplyClosure
-        , ("neml_apply_closure", var_arg_function_type i64 [|i64; i64|]) ) ]
-
-  let () =
-    Map.iter funcs ~f:(fun (id, typ) -> declare_function id typ lmod |> ignore)
+        , declare "neml_apply_closure" (var_arg_function_type i64 [|i64; i64|])
+        )
+      ; (PrintInt, declare "neml_print_int" (function_type i64 [|i64|])) ]
 
   let create_closure ~func ~argc bld =
-    let fcreate_id, fcreate_type = Map.find_exn funcs CreateClosure in
-    let fcreate = lookup_function fcreate_id lmod |> Option.value_exn in
+    let fcreate, fcreate_type = Map.find_exn funcs CreateClosure in
     build_call fcreate_type fcreate [|func; const_int i64 argc|] "r" bld
 
   let apply_closure ~closure ~args bld =
-    let fapply_id, fapply_type = Map.find_exn funcs ApplyClosure in
-    let fapply = lookup_function fapply_id lmod |> Option.value_exn in
+    let fapply, fapply_type = Map.find_exn funcs ApplyClosure in
     let argc = List.length args in
     build_call fapply_type fapply
       (Array.of_list (closure :: const_int i64 argc :: args))
       "r" bld
+
+  let print_int value bld =
+    let fprint, fprint_type = Map.find_exn funcs PrintInt in
+    build_call fprint_type fprint [|value|] "r" bld
 end
